728bcced03c48358ba562b5aedbd029f8c1f3434
KennethHaugland <35127961+KennethHaugland@users.noreply.github.com>
2024-10-12 21:56:12 +0200

    Add files via upload

---
+++ Wpf_glTF_testing/Wpf_glTF_testing/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using glTFLoader.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Converters;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_glTF_testing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public AxisAngleRotation3D rot_x { get; set; }
+        public AxisAngleRotation3D rot_y { get; set; }
+        public AxisAngleRotation3D rot_z { get; set; }
+        public ScaleTransform3D zoom { get; set; } = new ScaleTransform3D(0.1d,0.1d,0.1d);
+        public TranslateTransform3D center { get; set; }
+
+
+        public string[] planets = { 
+                // 0
+                "Sun_1_1391000.glb", 
+                // 1
+                "Mercury_1_4878.glb", 
+                // 2
+                "Venus_1_12103.glb", 
+                // 3
+                "Earth_1_12756.glb",
+                // 4
+                "Mars_1_6792.glb",
+                // 5
+                "Jupiter_1_142984.glb",
+                // 6
+                "Saturn_1_120536.glb",
+                // 7
+                "Uranus_1_51118.glb",
+                // 8
+                "Neptune_1_49528.glb",
+                // 9
+                "Pluto_1_2374.glb"};
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+          
+            PlanetSelect.ItemsSource = planets;
+            var TheCurrentPlanet = new Planet(planets[0]);
+            viewport3D1.Children.Add(TheCurrentPlanet.Visualisation);
+            
+            center = new TranslateTransform3D(0, 0, 0);
+            rot_x = new AxisAngleRotation3D(
+                new Vector3D(1, 0, 0),
+                slider_x.Value);
+            rot_y = new AxisAngleRotation3D(
+                new Vector3D(0, 1, 0),
+                slider_y.Value);
+            rot_z = new AxisAngleRotation3D(
+                new Vector3D(0, 0, 1),
+                slider_z.Value);
+            zoom = new ScaleTransform3D(
+                slider_zoom.Value,
+                slider_zoom.Value,
+                slider_zoom.Value);
+
+            Transform3DGroup t = new Transform3DGroup();
+
+            t.Children.Add(zoom);
+
+            // the order of the following three is significant
+            t.Children.Add(new RotateTransform3D(rot_y));
+            t.Children.Add(new RotateTransform3D(rot_x));
+            t.Children.Add(new RotateTransform3D(rot_z));
+
+            t.Children.Add(center);
+
+            viewport3D1.Camera.Transform = t;
+        }
+
+        void slider_x_changed(object sender, RoutedEventArgs args)
+        {
+            rot_x.Angle = slider_x.Value;
+        }
+        void slider_y_changed(object sender, RoutedEventArgs args)
+        {
+            rot_y.Angle = slider_y.Value;
+        }
+        void slider_z_changed(object sender, RoutedEventArgs args)
+        {
+            rot_z.Angle = slider_z.Value;
+        }
+
+        void slider_zoom_changed(object sender, RoutedEventArgs args)
+        {
+                zoom.ScaleY = slider_zoom.Value;
+                zoom.ScaleX = slider_zoom.Value;
+                zoom.ScaleZ = slider_zoom.Value;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = PlanetSelect.SelectedIndex;
+            var gg = new Planet(planets[index]);
+
+            viewport3D1.Children.Clear();
+            viewport3D1.InvalidateVisual();
+            DirectionalLight myDirectionalLight = new DirectionalLight();
+            myDirectionalLight.Color = Colors.White;
+            myDirectionalLight.Direction = new Vector3D(-1, -1, -1);
+            viewport3D1.Children.Add(new ModelVisual3D() { Content = myDirectionalLight });
+            viewport3D1.Children.Add(gg.Visualisation);
+            viewport3D1.InvalidateVisual();
+        }
+    }
+}
---
+++ Wpf_glTF_testing/Wpf_glTF_testing/Planet.cs
@@ -0,0 +1,213 @@
+﻿using glTFLoader.Schema;
+using glTFLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Wpf_glTF_testing
+{
+    public class Planet
+    {
+        public UInt32 Magic { get; private set; }
+
+        public UInt32 Version { get; private set; }
+
+        public UInt32 TotalFileLength { get; private set; }
+
+        public UInt32 JsonChuckLength { get; private set; }
+        public UInt32 BinChuckLength { get; private set; }
+
+        public byte[] JSON_data { get; private set; }
+        public byte[] BIN_data { get; private set; }
+
+        public Point3DCollection MaterialPoints { get; private set; } = new Point3DCollection();    
+
+        public Vector3DCollection NormalPoints { get; private set; } = new Vector3DCollection();  
+
+        public PointCollection TexturePoints { get; private set; } = new PointCollection();
+
+        public Int32Collection Indecies { get; private set; } = new Int32Collection();
+
+        public MeshGeometry3D Model3D { get; private set; }
+
+        public GeometryModel3D GeoModel3D { get;set; } = new GeometryModel3D();
+
+        public ModelVisual3D Visualisation { get; set; } = new ModelVisual3D();
+
+        public List<BitmapImage> Images { get; private set; } = new List<BitmapImage>();
+
+        public Planet(string filename)
+        {
+            // Get all metadata
+            Gltf glTFFile = Interface.LoadModel(filename);
+
+            // The different attributes in a reverse dictionary
+            Dictionary<string,int> attributes = glTFFile.Meshes[0].Primitives[0].Attributes;
+            Dictionary<int, string> AttrebutesIndex = attributes.ToDictionary(x => x.Value, x => x.Key);
+
+
+            // Load all byte arrays from the Binary file glTF version 2
+            using (var stream = File.Open(filename, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                {
+                    // Reading the inital data that determines the file type
+                    Magic = reader.ReadUInt32();
+                    Version = reader.ReadUInt32();
+                    TotalFileLength  = reader.ReadUInt32();
+
+                    // Read the JSON data
+                    JsonChuckLength = reader.ReadUInt32();
+                    UInt32 chunckType = reader.ReadUInt32();
+                    // Should be equal to JSON_hex 0x4E4F534A;         
+                    string hexValue = chunckType.ToString("X");
+
+                    JSON_data = reader.ReadBytes((int)JsonChuckLength);
+
+                    // Read teh binary data
+                    BinChuckLength = reader.ReadUInt32();
+                    UInt32 chunckType2 = reader.ReadUInt32();
+                    // Should be equal to BIN_hex 0x004E4942;
+                    string hexValue2 = chunckType2.ToString("X");
+                    
+                    BIN_data = reader.ReadBytes((int)BinChuckLength);
+                }
+            }
+
+            for (int i = 0; i < glTFFile.Accessors.Count(); i++)
+            {
+                Accessor CurrentAccessor = glTFFile.Accessors[i];
+
+                // Read the byte positions and offsets for each accessors
+                var BufferViewIndex = CurrentAccessor.BufferView;
+                BufferView BufferView = glTFFile.BufferViews[(int)BufferViewIndex];
+                var Offset = BufferView.ByteOffset;
+                var Length = BufferView.ByteLength;
+
+
+                // Check which type of accessor it is
+                string type = "";
+                if (AttrebutesIndex.ContainsKey(i))
+                    type = AttrebutesIndex[i];
+
+                if (type == "POSITION")
+                {
+                    // Used to scale all planets to +/- 1
+                    float[] ScalingFactorForVariables = new float[3];
+
+                    if (CurrentAccessor.Max == null)
+                        ScalingFactorForVariables = new float[3] { 1.0f, 1.0f, 1.0f };
+                    else
+                        ScalingFactorForVariables = CurrentAccessor.Max;
+
+                    // Upscaling factor
+                    float UpscalingFactor = 1.5f;
+
+                    Point3DCollection PointsPosisions = new Point3DCollection();
+
+                    for (int n = Offset; n < Offset + Length; n += 4)
+                    {
+                        float x = BitConverter.ToSingle(BIN_data, n) / ScalingFactorForVariables[0] * UpscalingFactor;
+                        n += 4;
+                        float y = BitConverter.ToSingle(BIN_data, n) / ScalingFactorForVariables[1] * UpscalingFactor;
+                        n += 4;
+                        float z = BitConverter.ToSingle(BIN_data, n) / ScalingFactorForVariables[2] * UpscalingFactor;
+
+                        PointsPosisions.Add(new Point3D(x, y, z));
+                    }
+                    MaterialPoints = PointsPosisions;
+                }
+                else if (type == "NORMAL")
+                {
+                    Vector3DCollection NormalsForPosisions = new Vector3DCollection();
+                    for (int n = Offset; n < Offset + Length; n += 4)
+                    {
+                        float x = BitConverter.ToSingle(BIN_data, n);
+                        n += 4;
+                        float y = BitConverter.ToSingle(BIN_data, n);
+                        n += 4;
+                        float z = BitConverter.ToSingle(BIN_data, n);
+
+                        NormalsForPosisions.Add(new Vector3D(x, y, z));
+                    }
+
+                    NormalPoints = NormalsForPosisions;
+                }
+                else if (type.Contains("TEXCOORD"))
+                {
+                    // Assuming texture posisions
+                    PointCollection vec2 = new PointCollection();
+                    for (int n = Offset; n < Offset + Length; n += 4)
+                    {
+                        double x = (double)BitConverter.ToSingle(BIN_data, n);
+                        n += 4;
+                        double y = (double)BitConverter.ToSingle(BIN_data, n);
+
+                        vec2.Add(new Point(x, y));
+                    }
+
+                    TexturePoints = vec2;
+                }
+                else
+                {
+                    if (CurrentAccessor.ComponentType == Accessor.ComponentTypeEnum.UNSIGNED_SHORT)
+                    {
+                        for (int n = Offset; n < Offset + Length; n += 2)
+                        {
+                            UInt16 TriangleItem = BitConverter.ToUInt16(BIN_data, n);
+                            Indecies.Add((Int32)TriangleItem);
+
+                        }
+                    }
+                }
+            }
+
+            // Showing the image
+            foreach (glTFLoader.Schema.Image item in glTFFile.Images)
+            {
+                //var ImageType = item.MimeType;
+                int BufferViewIndex = (int)item.BufferView;
+                BufferView BufferView = glTFFile.BufferViews[BufferViewIndex];
+                var Offset = BufferView.ByteOffset;
+                var Length = BufferView.ByteLength;
+
+                // Copy the relevant data from binary part
+                byte[] ImageBytes = new byte[Length];
+                Array.Copy(BIN_data, Offset, ImageBytes, 0, Length);
+
+                // Conmvert to image
+                MemoryStream ms = new MemoryStream(ImageBytes);
+                BitmapImage Img = new BitmapImage();
+                Img.BeginInit();
+                Img.StreamSource = ms;
+                Img.EndInit();
+                Images.Add(Img);             
+            }
+
+            // Construct the WPF 3D ViewPort model
+            Model3D = new MeshGeometry3D();
+            Model3D.TriangleIndices = Indecies;
+            Model3D.Positions = MaterialPoints;
+            Model3D.Normals = NormalPoints;
+            Model3D.TextureCoordinates = TexturePoints;
+
+            // Geometry model
+            GeoModel3D.Geometry = Model3D;
+
+            if(glTFFile.Meshes.Count()==1)
+                GeoModel3D.Material = new DiffuseMaterial() { Brush = new ImageBrush(Images[Images.Count-1]) };
+            else
+                GeoModel3D.Material = new DiffuseMaterial() { Brush = new ImageBrush(Images[0]) };
+            
+            // ModelVisual3D for showing this component
+            Visualisation.Content = GeoModel3D;
+        }
+    }
+}
